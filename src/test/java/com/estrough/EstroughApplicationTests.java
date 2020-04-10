273777ab154af7220c8e3debf36671d8af8f7aef
Hau Tran <hautranbkdn@gmail.com>
2020-04-10 15:55:04 +0700

    initing project

---
+++ src/test/java/com/estrough/EstroughApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estrough;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstroughApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
